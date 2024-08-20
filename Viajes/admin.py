0bc172570487353a690daaabb4b2ff811ef378fd
Blas <blaslaborde@gmail.com>
2024-08-20 19:40:29 -0300

    Agregue en admin los models

--- Viajes/admin.py
+++ Viajes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Viajes.models import Cliente, Destino, Estadia
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Destino)
+admin.site.register(Estadia)
